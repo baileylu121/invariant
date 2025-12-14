7a63a86a79a800f4abf4362e47a182375eb7fe8e
Luke Bailey <baileylu@tcd.ie>
2025-12-14 19:44:57 +0000

    Add ghostty systemd unit config + separate gui and tui home

--- users/luke.nix
+++ users/luke.nix
@@ -7,7 +7,7 @@ in
     home-manager.flakeModules.home-manager
   ];
 
-  flake.modules.homeManager.luke = {
+  flake.modules.homeManager.luke-tui = {
     imports = [
       self.modules.homeManager.neovim
       self.modules.homeManager.bash
@@ -28,6 +28,13 @@ in
     };
   };
 
+  flake.modules.homeManager.luke-gui = {
+    imports = [
+      self.modules.homeManager.luke-tui
+      self.modules.homeManager.ghostty
+    ];
+  };
+
   flake.modules.nixos.users-luke =
     { pkgs, ... }:
     {
@@ -42,18 +49,22 @@ in
         initialPassword = "4835";
       };
 
-      home-manager.users.luke = self.modules.homeManager.luke;
+      home-manager.users.luke = self.modules.homeManager.luke-gui;
 
       programs.niri.enable = true;
 
       environment.systemPackages = with pkgs; [
-        ghostty
         librewolf
       ];
     };
 
-  flake.homeConfigurations.luke = home-manager.lib.homeManagerConfiguration {
+  flake.homeConfigurations.luke-tui = home-manager.lib.homeManagerConfiguration {
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    modules = [ self.modules.homeManager.luke ];
+    modules = [ self.modules.homeManager.luke-tui ];
+  };
+
+  flake.homeConfigurations.luke-gui = home-manager.lib.homeManagerConfiguration {
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    modules = [ self.modules.homeManager.luke-gui ];
   };
 }
