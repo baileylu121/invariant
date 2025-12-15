27ec6676c6f4d490bfd257f9f51648c39ee44337
Luke Bailey <baileylu@tcd.ie>
2025-12-15 13:59:15 +0000

    Enable bluetooth

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -7,5 +7,19 @@
     networking.networkmanager.enable = true;
 
     networking.nftables.enable = true;
+
+    hardware.bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings = {
+        General = {
+          Experimental = true;
+          FastConnectable = true;
+        };
+        Policy = {
+          AutoEnable = true;
+        };
+      };
+    };
   };
 }
