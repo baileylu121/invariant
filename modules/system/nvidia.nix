165315b725916a9184ab789be12845036c2770ea
Luke Bailey <baileylu@tcd.ie>
2026-01-13 16:22:04 +0000

    Render on the intergrated gpu

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -4,20 +4,28 @@
     {
       hardware.graphics.enable = true;
 
-      services.xserver.videoDrivers = [ "nvidia" ];
+      services.xserver.videoDrivers = [
+        "amdgpu"
+        "nvidia"
+      ];
 
       hardware.nvidia = {
         modesetting.enable = true;
 
-        powerManagement.enable = false;
-        powerManagement.finegrained = false;
+        powerManagement.enable = true;
+        powerManagement.finegrained = true;
 
-        open = false;
+        open = true;
 
         nvidiaSettings = true;
 
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        package = config.boot.kernelPackages.nvidiaPackages.latest;
 
+        prime = {
+          offload.enable = true;
+          amdgpuBusId = "PCI:101:0:0";
+          nvidiaBusId = "PCI:100:0:0";
+        };
       };
 
       hardware.nvidia-container-toolkit.enable = true;
