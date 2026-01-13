3edbb74d0b49afde69e2d6d358cbf660e6a2fe39
Luke Bailey <baileylu@tcd.ie>
2026-01-13 17:03:32 +0000

    Disable unused microphone from monitor

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -3,6 +3,23 @@
     services.pipewire = {
       enable = true;
       pulse.enable = true;
+      extraConfig."pipewire-pulse"."50-disable-family-mic.conf" = {
+        "pulse.rules" = [
+          {
+            matches = [
+              {
+                "media.class" = "Audio/Source";
+                "node.description" = "Family 17h/19h/1ah HD Audio Controller Analog Stereo";
+              }
+            ];
+            actions = {
+              update-props = {
+                "node.disabled" = true;
+              };
+            };
+          }
+        ];
+      };
     };
   };
 }
