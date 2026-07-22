6f53a7439a97886ea776786eff727f6cfd4c4fbb
Luke Bailey <baileylu@tcd.ie>
2026-07-22 17:27:18 +0100

    Persist `tailscale` info

--- modules/system/btrfs/impermenance.nix
+++ modules/system/btrfs/impermenance.nix
@@ -12,6 +12,7 @@
         "/var/log"
         "/var/lib/bluetooth"
         "/var/lib/nixos"
+        "/var/lib/tailscale"
         "/var/lib/systemd/coredump"
         "/etc/NetworkManager/system-connections"
         "/etc/nixos"
